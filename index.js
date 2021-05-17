99e5a5a6560436cd9c0b4bace3b2dee3edec3e63
kasech-isaac <63433163+kasech-isaac@users.noreply.github.com>
2021-05-17 13:31:50 -0700

    make it better

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.ge('/', (req, res)=>{
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res)=>{
   res.send('Hello to Memories API')
 })
-app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
